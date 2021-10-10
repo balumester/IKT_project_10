7abc20e8fc8330abddbe42cd9dbaa62d1cd8e7d8
balumester <90311852+balumester@users.noreply.github.com>
2021-10-10 16:28:28 +0200

    Create 5.feladat.py

---
+++ 5.feladat.py
@@ -0,0 +1,5 @@
+print('5.feladat')
+szám1=int(input('Adj meg egy számot:'))
+szám2=int(input('Adj meg egy másik számot:'))
+szám3=int(input('Adj meg egy harmadik számot:'))
+print('A 3 szám összege:',szám1+szám2+szám3)
